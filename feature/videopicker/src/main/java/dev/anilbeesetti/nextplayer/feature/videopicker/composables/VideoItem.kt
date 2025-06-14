021fe96b37cbea663525a53f770c7f4599b7bc43
bermount <127654770+bermount@users.noreply.github.com>
2025-06-14 14:43:00 +0900

    Update VideoItem.kt

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/VideoItem.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/VideoItem.kt
@@ -133,16 +133,16 @@ fun VideoItem(
                 overflow = TextOverflow.Ellipsis,
             )
         },
-        supportingContent = {
-            if (preferences.showPathField) {
-                Text(
-                    text = video.path.substringBeforeLast("/"),
-                    maxLines = 1,
-                    style = MaterialTheme.typography.bodySmall.copy(fontSize = 7.sp),
-                    overflow = TextOverflow.Ellipsis,
-                    modifier = Modifier.padding(vertical = 2.dp),
-                )
-            }
+#        supportingContent = {
+#            if (preferences.showPathField) {
+#                Text(
+#                    text = video.path.substringBeforeLast("/"),
+#                    maxLines = 1,
+#                    style = MaterialTheme.typography.bodySmall.copy(fontSize = 9.sp),
+#                    overflow = TextOverflow.Ellipsis,
+#                    modifier = Modifier.padding(vertical = 1.dp),
+#                )
+#            }
             FlowRow(
                 modifier = Modifier
                     .fillMaxWidth()
