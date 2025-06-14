c0996bbdc2571dd615657d5acdf254fa7fda4927
bermount <127654770+bermount@users.noreply.github.com>
2025-06-14 11:33:42 +0900

    Update VideoItem.kt

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/VideoItem.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/composables/VideoItem.kt
@@ -104,7 +104,7 @@ fun VideoItem(
                 if (preferences.showPlayedProgress && video.playedPercentage > 0) {
                     Box(
                         modifier = Modifier
-                            .height(4.1.dp)
+                            .height(8.dp)
                             .fillMaxWidth()
                             .align(Alignment.BottomCenter),
                     ) {
