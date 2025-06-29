be9fcd50b03153143ca71575c4fb68ba35a722e4
bermount <127654770+bermount@users.noreply.github.com>
2025-06-29 14:18:49 +0900

    Update MediaPickerViewModel.kt

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/screens/media/MediaPickerViewModel.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/screens/media/MediaPickerViewModel.kt
@@ -82,10 +82,14 @@ class MediaPickerViewModel @Inject constructor(
             mediaService.renameMedia(uri, to)
         }
     }
-
+    
     fun onRefreshClicked() {
         viewModelScope.launch {
             uiStateInternal.update { it.copy(refreshing = true) }
+            val syncDirUri = preferencesRepository.playerPreferences.first().syncPlaybackPositionsFolderUri
+            if (!syncDirUri.isNullOrBlank()) {
+                mediaRepository.syncAllJsonPlaybackPositions(syncDirUri)
+            }
             mediaSynchronizer.refresh()
             uiStateInternal.update { it.copy(refreshing = false) }
         }
