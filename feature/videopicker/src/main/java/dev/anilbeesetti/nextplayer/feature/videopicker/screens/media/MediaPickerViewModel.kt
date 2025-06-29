b1704fb075b897d533858b349d0e7ca7e2d0d015
bermount <127654770+bermount@users.noreply.github.com>
2025-06-29 14:45:48 +0900

    Update MediaPickerViewModel.kt

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/screens/media/MediaPickerViewModel.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/screens/media/MediaPickerViewModel.kt
@@ -21,6 +21,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.first // <-- Add this import
 
 @HiltViewModel
 class MediaPickerViewModel @Inject constructor(
