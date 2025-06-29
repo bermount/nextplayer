2e5c0e3231e7ae173a885c77c4431e5cfd3166fd
bermount <127654770+bermount@users.noreply.github.com>
2025-06-29 15:40:26 +0900

    Update MediaPickerFolderViewModel.kt

--- feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/screens/mediaFolder/MediaPickerFolderViewModel.kt
+++ feature/videopicker/src/main/java/dev/anilbeesetti/nextplayer/feature/videopicker/screens/mediaFolder/MediaPickerFolderViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.anilbeesetti.nextplayer.core.data.repository.PreferencesRepository
 import dev.anilbeesetti.nextplayer.core.data.repository.MediaRepository
-import dev.anilbeesetti.nextplayer.core.data.repository.LocalMediaRepository
 import dev.anilbeesetti.nextplayer.core.domain.GetSortedMediaUseCase
 import dev.anilbeesetti.nextplayer.core.media.services.MediaService
 import dev.anilbeesetti.nextplayer.core.media.sync.MediaInfoSynchronizer
@@ -93,7 +92,7 @@ class MediaPickerFolderViewModel @Inject constructor(
             uiStateInternal.update { it.copy(refreshing = true) }
             val syncDirUri = preferencesRepository.playerPreferences.first().syncPlaybackPositionsFolderUri
             if (!syncDirUri.isNullOrBlank()) {
-                localMediaRepository.syncAllJsonPlaybackPositions(syncDirUri)
+                mediaRepository.syncAllJsonPlaybackPositions(syncDirUri)
             }
             mediaSynchronizer.refresh()
             uiStateInternal.update { it.copy(refreshing = false) }
