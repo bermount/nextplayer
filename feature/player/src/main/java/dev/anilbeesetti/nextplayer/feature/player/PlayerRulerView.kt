9d424c275a0a716153e851b8ea9af2e8d2e21eb8
bermount <127654770+bermount@users.noreply.github.com>
2025-07-03 16:03:28 +0900

    Update PlayerRulerView.kt

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerRulerView.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerRulerView.kt
@@ -23,32 +23,33 @@ class PlayerRulerView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
+        
         if (durationMs <= 0) return
-
+        
         val widthPx = width.toFloat()
         val widthPerMs = widthPx / durationMs
-
+        
         val baseLineHeight = thinBarHeightPx
         val longLineHeight = baseLineHeight * 1.5f
-
+        
         val baseLineWidth = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP, 1f, resources.displayMetrics
         )
         val longLineWidth = baseLineWidth * 2
-
-        // Draw tick marks every 10 minutes (600000 ms), longer every 30 minutes
-        var markerMs = durationMs - 10 * 60_000L
+        
+        // Start from the right edge (durationMs) and go backward
+        var markerMs = durationMs
         while (markerMs > 0) {
-            val isLong = ((markerMs / (10 * 60_000L)) % 3 == 0L)
-            val x = widthPx - (markerMs * widthPerMs)  // right to left
-
-            linePaint.strokeWidth = if (isLong) longLineWidth else baseLineWidth
-            val lineHeight = if (isLong) longLineHeight else baseLineHeight
-
-            canvas.drawLine(
-                x, 0f, x, lineHeight, linePaint
-            )
+            if (markerMs != durationMs) {
+                val isLong = ((markerMs / (10 * 60_000L)) % 3 == 0L)
+                val x = widthPx - (markerMs * widthPerMs)
+                linePaint.strokeWidth = if (isLong) longLineWidth else baseLineWidth
+                val lineHeight = if (isLong) longLineHeight else baseLineHeight
+                
+                canvas.drawLine(
+                    x, 0f, x, lineHeight, linePaint
+                )
+            }
             markerMs -= 10 * 60_000L
         }
     }
