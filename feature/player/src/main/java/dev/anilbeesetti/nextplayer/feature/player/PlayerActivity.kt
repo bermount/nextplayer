2be0a00800ebf60bf2d0c8983ac6da64e378b14f
bermount <127654770+bermount@users.noreply.github.com>
2025-07-04 11:31:38 +0900

    Update PlayerActivity.kt

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/PlayerActivity.kt
@@ -397,12 +397,20 @@ class PlayerActivity : AppCompatActivity() {
             subtitleFileLauncherLaunchedForMediaItem = null
         }
         initializePlayerView()
+        
         //Remaining Time Text
         finishTimeText = findViewById(R.id.finish_time_text)
         remainingTimeText = findViewById(R.id.remaining_time_text)
-        
-        val screenWidth = resources.displayMetrics.widthPixels
-        val scaledTextSizePx = screenWidth / 50f // Adjust divisor for your preferred size
+
+        val displayMetrics = DisplayMetrics()
+        (context?.getSystemService(Context.WINDOW_SERVICE) as? WindowManager)?.defaultDisplay?.getMetrics(displayMetrics)
+
+        //Convert screen width to centimeters
+        val screenWidthInches = displayMetrics.widthPixels / displayMetrics.xdpi
+        val screenWidthCm = screenWidthInches * 2.54f
+
+        val scaledTextSizeCm = screenWidthCm * 0.06f
+        val scaledTextSizePx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_CM, scaledTextSizeCm, displayMetrics)
         val scaledTextSizeSp = scaledTextSizePx / resources.displayMetrics.scaledDensity
         
         finishTimeText.setTextSize(TypedValue.COMPLEX_UNIT_SP, scaledTextSizeSp)
